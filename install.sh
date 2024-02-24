97dd29f0f2f002ea291cfb87a21b9b04868af82e
JULIEN SOULE <julien.soule@hotmail.fr>
2024-02-25 00:23:57 +0100

    changed configuration

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ mkdir python-envs;
 cd python-envs;
 python3 -m venv pistonball;
 source pistonball/bin/activate;
+cd ..;
 python3 -m pip install -r requirements.txt;
 python3 -m pip install "ray[tune]";
 echo "Use the following command to launch pistonball simulation with the proper python environment";
