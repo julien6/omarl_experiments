3574d5207ab3250a73e5da7c7d22de08b7989797
Julien Soule <julien.soule@lcis.grenoble-inp.fr>
2024-02-24 12:36:18 +0100

    modified installation

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ source pistonball/bin/activate;
 pip install -r requirements.txt;
 pip install "ray[tune]";
 echo "Use the following command to launch pistonball simulation with the proper python environment";
-echo "'source $./python-envs/pistonball/bin/activate'"
+echo "'source ./python-envs/pistonball/bin/activate'"
