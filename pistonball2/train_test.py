0b0d88116e1338523424a08fca5f2186f99bcd33
Julien Soule <julien.soule@lcis.grenoble-inp.fr>
2024-03-04 01:01:24 +0100

    continued AIAI

--- pistonball2/train_test.py
+++ pistonball2/train_test.py
@@ -6,9 +6,12 @@ import os
 from array2gif import write_gif
 import numpy as np
 import matplotlib.pyplot as plt
+from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
+from stable_baselines3.common.logger import configure
 
 if not os.path.exists("./policy.zip"):
     print("Training")
+
     env = pistonball_v6.parallel_env(n_pistons=20, time_penalty=-0.1, continuous=True, random_drop=True,
                                      random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=125)
     env = ss.color_reduction_v0(env, mode='B')
@@ -17,12 +20,27 @@ if not os.path.exists("./policy.zip"):
     env = ss.pettingzoo_env_to_vec_env_v1(env)
     env = ss.concat_vec_envs_v1(
         env, 8, num_cpus=4, base_class='stable_baselines3')
+    
+    # Stop training when the model reaches the reward threshold
+    callback_on_best = StopTrainingOnRewardThreshold(reward_threshold=-200, verbose=1)
+    eval_callback = EvalCallback(env, callback_on_new_best=callback_on_best, verbose=1)
+    
     model = PPO(CnnPolicy, env, verbose=3, gamma=0.95, n_steps=256, ent_coef=0.0905168, learning_rate=0.00062211,
                 vf_coef=0.042202, max_grad_norm=0.9, gae_lambda=0.99, n_epochs=5, clip_range=0.3, batch_size=256)
-    model.learn(total_timesteps=2000000)
+
+    if not os.path.exists("./sb3_logs"):
+        os.makedirs("./sb3_logs")
+    tmp_path = "./sb3_logs"
+    # set up logger
+    new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+    model.set_logger(new_logger)
+
+    # Almost infinite number of timesteps, but the training will stop
+    # early as soon as the reward threshold is reached
+    model.learn(total_timesteps=2000000, callback=eval_callback)
     model.save("policy")
 
-# Rendering
+
 print("Rendering")
 env = pistonball_v6.env(render_mode="rgb_array", n_pistons=20, time_penalty=-0.1, continuous=True, random_drop=True,
                         random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=125)
@@ -55,16 +73,17 @@ for i in range(NUM_RESETS):
                 env.render(), axes=(1, 0, 2)))
 
 env.close()
-print({agent: [int(act[0]) if act is not None else 0 for _,
-               act, _ in data] for agent, data in trajectories.items()})
-print("average total reward: ", total_reward/NUM_RESETS)
-write_gif(obs_list, 'pistonball_test.gif', fps=15)
 
-# Plotting
-rewards = model.episode_reward
-plt.plot(rewards)
-plt.title('Évolution de la Récompense en fonction des Itérations')
-plt.xlabel('Itérations')
-plt.ylabel('Récompense')
-plt.savefig('rewards.png')
-# plt.show()
+# print({agent: [int(act[0]) if act is not None else 0 for _,
+#                act, _ in data] for agent, data in trajectories.items()})
+# print("average total reward: ", total_reward/NUM_RESETS)
+# write_gif(obs_list, 'pistonball_test.gif', fps=15)
+
+# # Plotting
+# rewards = model.logger.
+# plt.plot(rewards)
+# plt.title('Évolution de la Récompense en fonction des Itérations')
+# plt.xlabel('Itérations')
+# plt.ylabel('Récompense')
+# plt.savefig('rewards.png')
+# # plt.show()
