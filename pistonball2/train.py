cabdae954cb7ef858664c57536c7209312c5815b
Julien <julien.soule@lcis.grenoble-inp.fr>
2024-03-06 13:51:28 +0100

    continued AIAI

--- pistonball2/train.py
+++ pistonball2/train.py
@@ -26,13 +26,13 @@ def main():
 
     env = pistonball_v6.parallel_env(n_pistons=20, time_penalty=-0.1, continuous=True, random_drop=True,
                                      random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=125)
-
+    env.reset(42)
     env = ss.color_reduction_v0(env, mode='B')
     env = ss.resize_v1(env, x_size=84, y_size=84)
     env = ss.frame_stack_v1(env, 3)
     env = ss.pettingzoo_env_to_vec_env_v1(env)
     env = ss.concat_vec_envs_v1(
-        env, 8, num_cpus=8, base_class='stable_baselines3')
+        env, 8, num_cpus=4, base_class='stable_baselines3')
 
     # Stop training when the model reaches the reward threshold
     callback_on_best = StopTrainingOnRewardThreshold(
@@ -79,6 +79,7 @@ def main():
     print("Rendering")
     env = pistonball_v6.env(render_mode="rgb_array", n_pistons=20, time_penalty=-0.1, continuous=True, random_drop=True,
                             random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=125)
+    env.reset(42)
     env = ss.color_reduction_v0(env, mode='B')
     env = ss.resize_v1(env, x_size=84, y_size=84)
     env = ss.frame_stack_v1(env, 3)
