63964382de9a13c6cc84a00c807c2a482099c534
Julien <julien.soule@lcis.grenoble-inp.fr>
2024-03-13 17:40:02 +0100

    continued OMARL

--- pistonball2/moonlanding.py
+++ pistonball2/moonlanding.py
@@ -1,40 +1,47 @@
 import gymnasium as gym
 
-from stable_baselines3 import DQN
-from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.utils import set_random_seed
 
 
-# Create environment
-env = gym.make("LunarLander-v2", render_mode="rgb_array")
+def make_env(env_id: str, rank: int, seed: int = 0):
+    """
+    Utility function for multiprocessed env.
 
-# Instantiate the agent
-model = DQN("MlpPolicy", env, verbose=1)
-# Train the agent and display a progress bar
-model.learn(total_timesteps=int(2e5), progress_bar=True)
-# Save the agent
-model.save("dqn_lunar")
-del model  # delete trained model to demonstrate loading
+    :param env_id: the environment ID
+    :param num_env: the number of environments you wish to have in subprocesses
+    :param seed: the inital seed for RNG
+    :param rank: index of the subprocess
+    """
+    def _init():
+        env = gym.make(env_id, render_mode="human")
+        env.reset(seed=seed + rank)
+        return env
+    set_random_seed(seed)
+    return _init
 
-# Load the trained agent
-# NOTE: if you have loading issue, you can pass `print_system_info=True`
-# to compare the system on which the model was trained vs the current one
-# model = DQN.load("dqn_lunar", env=env, print_system_info=True)
-model = DQN.load("dqn_lunar", env=env)
 
-# Evaluate the agent
-# NOTE: If you use wrappers with your environment that modify rewards,
-#       this will be reflected here. To evaluate with original rewards,
-#       wrap environment in a "Monitor" wrapper before other wrappers.
-mean_reward, std_reward = evaluate_policy(
-    model, model.get_env(), n_eval_episodes=10)
+if __name__ == "__main__":
+    env_id = "CartPole-v1"
+    num_cpu = 4  # Number of processes to use
+    # Create the vectorized environment
+    vec_env = SubprocVecEnv([make_env(env_id, i) for i in range(num_cpu)])
 
-# Enjoy trained agent
-vec_env = model.get_env()
-obs = vec_env.reset()
-for i in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, rewards, dones, info = vec_env.step(action)
-    vec_env.render("human")
+    # Stable Baselines provides you with make_vec_env() helper
+    # which does exactly the previous steps for you.
+    # You can choose between `DummyVecEnv` (usually faster) and `SubprocVecEnv`
+    # env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
+
+    model = PPO("MlpPolicy", vec_env, verbose=1)
+    model.learn(total_timesteps=25_000)
+
+    obs = vec_env.reset()
+    for _ in range(1000):
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = vec_env.step(action)
+        vec_env.render()
 
 # ===========
 
