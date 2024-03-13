0bce48481030eecfbf135346122b9826359fda75
Julien <julien.soule@lcis.grenoble-inp.fr>
2024-03-13 17:39:36 +0100

    continued OMARL

--- pistonball2/moonlanding.py
+++ pistonball2/moonlanding.py
@@ -1,15 +1,40 @@
 import gymnasium as gym
 
-from stable_baselines3 import SAC
-from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3 import DQN
+from stable_baselines3.common.evaluation import evaluate_policy
 
-vec_env = make_vec_env("Pendulum-v1", n_envs=4, seed=0)
 
-# We collect 4 transitions per call to `ènv.step()`
-# and performs 2 gradient steps per call to `ènv.step()`
-# if gradient_steps=-1, then we would do 4 gradients steps per call to `ènv.step()`
-model = SAC("MlpPolicy", vec_env, train_freq=1, gradient_steps=2, verbose=1)
-model.learn(total_timesteps=10_000)
+# Create environment
+env = gym.make("LunarLander-v2", render_mode="rgb_array")
+
+# Instantiate the agent
+model = DQN("MlpPolicy", env, verbose=1)
+# Train the agent and display a progress bar
+model.learn(total_timesteps=int(2e5), progress_bar=True)
+# Save the agent
+model.save("dqn_lunar")
+del model  # delete trained model to demonstrate loading
+
+# Load the trained agent
+# NOTE: if you have loading issue, you can pass `print_system_info=True`
+# to compare the system on which the model was trained vs the current one
+# model = DQN.load("dqn_lunar", env=env, print_system_info=True)
+model = DQN.load("dqn_lunar", env=env)
+
+# Evaluate the agent
+# NOTE: If you use wrappers with your environment that modify rewards,
+#       this will be reflected here. To evaluate with original rewards,
+#       wrap environment in a "Monitor" wrapper before other wrappers.
+mean_reward, std_reward = evaluate_policy(
+    model, model.get_env(), n_eval_episodes=10)
+
+# Enjoy trained agent
+vec_env = model.get_env()
+obs = vec_env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, rewards, dones, info = vec_env.step(action)
+    vec_env.render("human")
 
 # ===========
 
