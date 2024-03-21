7de5529f3e589bdb4a2df31596656d5e410d7bed
Julien <julien.soule@lcis.grenoble-inp.fr>
2024-03-21 17:29:59 +0100

    continued OMARL

--- misc/test.py
+++ misc/test.py
@@ -4,6 +4,11 @@ import numpy
 import json
 import matplotlib.pyplot as plt
 import csv
+import numpy as np
+
+from sklearn.cluster import AgglomerativeClustering
+from scipy.cluster.hierarchy import dendrogram, linkage
+
 
 # s1 = [1,2,0,1,2,4,1,3,4,3,2,1,5,4]
 # s2 = [0,0,1,7,2,3,4,9,5,3,9,7]
@@ -79,44 +84,47 @@ def find_subsequences_with_best_lcs(sequences: List[List[int]]):
         seqs.append(lcs)
     return lcs, seq1, seq2
 
+def sequence_dist(seq1, seq2):
+    lcs = longest_common_subsequence(seq1, seq2)
+    return 1 - (2 * len(lcs) / (len(seq1)+len(seq2)))
+
 actions = {'piston_0': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_1': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_2': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 0], 'piston_3': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 0], 'piston_4': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, -1, 0], 'piston_5': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, 0], 'piston_6': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, 0], 'piston_7': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_8': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_9': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_10': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_11': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_12': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_13': [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_14': [-1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_15': [-1, -1, -1, -1, -1, -1, -1, 1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_16': [-1, -1, -1, -1, -1, -1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_17': [-1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_18': [1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0], 'piston_19': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]}
 
 acts = [a for p_name, a in actions.items()]
 
-s1 = [1,2,0,1,2,4,1,3,4,3,2,1,5,4,4,6]
-s2 = [0,0,1,7,2,3,4,9,5,3,9,7,6]
-s3 = [1,8,2,3,6,4,11,5,0,0,0,6,0,1,2]
-s4 = [0,1,41,2,3,6,4,5,14,9,6,6,7,0,0,0]
+s1 = [1,2,4,5,6,0,4,0,1,0,0]
+s2 = [1,2,3,4,5,6,0,0,0,0,0]
+s3 = [1,8,2,3,6,4,1,5,0,0,0]
+s4 = [0,1,4,1,2,3,6,4,5,1,9]
 s5 = [5,4,3,2,1,0,1,2,3,4,5]
-s6 = [5,0,0,4,2,2,1,3,7,47,2,6,1,9,0,63,1,2,3,4,5]
+s6 = [5,0,0,4,2,2,1,3,7,4,2]
+
+# Matrice de similarité
 
 # sequences = acts
-sequences = [s1, s2, s3, s4, s5, s6]
+sequences = [s1, s2, s3, s4, s5, s6, s6]
 # result = longest_common_subsequence_multiple(sequences)
 # result, s1, s2 = find_two_subsequences_with_best_lcs(sequences)
 # print("La sous-séquence commune la plus grande est :", result, " pour les sequences: ", s1, " et ", s2)
 
-lcs, seq1, seq2 = find_two_subsequences_with_best_lcs(sequences)
-print(lcs)
+distance_matrix = [[0] * len(sequences) for _ in sequences]
+for i, seq1 in enumerate(sequences):
+    for j, seq2 in enumerate(sequences):
+        distance_matrix[i][j] = sequence_dist(seq1, seq2)
 
-sequences.remove(seq1)
-sequences.remove(seq2)
+distance_matrix = np.array(distance_matrix)
 
-lcs, seq1, seq2 = find_two_subsequences_with_best_lcs(sequences)
-print(lcs)
+print(distance_matrix)
 
-sequences.remove(seq1)
-sequences.remove(seq2)
+# Calcul du clustering hiérarchique avec la méthode de liaison complète
+Z = linkage(distance_matrix, method='complete')
 
-lcs, seq1, seq2 = find_two_subsequences_with_best_lcs(sequences)
-print(lcs)
+print(Z)
 
-sequences.remove(seq1)
-sequences.remove(seq2)
-
-lcs, seq1, seq2 = find_two_subsequences_with_best_lcs(sequences)
-print(lcs)
-
-# Trouver les 2 séquences donnant le lcs pour toutes les séquences
-# Enlever les 2 séquences
-# 
+# Affichage du dendrogramme
+plt.figure(figsize=(10, 5))
+dendrogram(Z)#, labels=["s1", "s2", "s3", "s4", "s5", "s6"])
+plt.title('Dendrogramme')
+plt.xlabel('Index des échantillons')
+plt.ylabel('Distance')
+plt.show()
