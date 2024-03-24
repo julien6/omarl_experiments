3b87205779e1bf54846753e6b38a48c0b1fd7519
Julien Soule <julien.soule@lcis.grenoble-inp.fr>
2024-03-25 00:31:11 +0100

    continued OMARL

--- envs/mcy/run_train.sh
+++ envs/mcy/run_train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#OAR -n Pistonball
-#OAR -l /nodes=1/gpu=1,walltime=0:30:00
+#OAR -n MovingCompany
+#OAR -l /nodes=1/gpu=1,walltime=4:00:00
 #OAR --stdout %jobid%.out
 #OAR --stderr %jobid%.err
 #OAR --project pr-ai4cmas
 #OAR -p gpumodel='V100'
 
 cd /bettik/soulej/omarl_experiments/
-source ./python-envs/pistonball/bin/activate
-python3 pistonball_train.py
+source ./python-envs/mcy/bin/activate
+python3 mcy_train_test.py
