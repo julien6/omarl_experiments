89a13aa618aab962fabfe776b4376921bcd922bb
JULIEN SOULE <julien.soule@hotmail.fr>
2024-04-22 14:05:34 +0200

    continued omarl

--- llm_plugin/test.py
+++ llm_plugin/test.py
@@ -1,9 +1,10 @@
+import sys
 from transformers import pipeline
 
 # Charger le modèle
 generator = pipeline("text-generation", model="EleutherAI/gpt-neo-2.7B")
 
-prompt_command = "Could you write an historical overview of the Americas's colonization"
+prompt_command = sys.argv[1] if sys.argv[1] is not None else "What is a castle?"
 
 # Générer une réponse en utilisant le prompt
 response = generator(prompt_command, max_length=400)
