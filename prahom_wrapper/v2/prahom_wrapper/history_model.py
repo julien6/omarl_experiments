a26c21cc02c5a9e91841fd786d16cae63a83bbd7
julien soulé <julien.soule@lcis.grenoble-inp.fr>
2024-05-24 02:58:21 +0200

    continued omarl

--- prahom_wrapper/v2/prahom_wrapper/history_model.py
+++ prahom_wrapper/v2/prahom_wrapper/history_model.py
@@ -2,23 +2,24 @@ import sys
 sys.path
 sys.path.append('../../../.')
 
-import copy
-from dataclasses import dataclass, field
-import dataclasses
-from enum import Enum
-import itertools
-import json
-import random
-from typing import Any, Callable, Dict, List, Tuple, Union
-import networkx as nx
-import matplotlib.pyplot as plt
-from pprint import pprint
-import re
-
-import numpy as np
-from prahom_wrapper.organizational_model import cardinality, organizational_model, os_encoder
-from PIL import Image
 from prahom_wrapper.utils import draw_networkx_edge_labels
+from PIL import Image
+from prahom_wrapper.organizational_model import cardinality, organizational_model, os_encoder
+import numpy as np
+import re
+from pprint import pprint
+import matplotlib.pyplot as plt
+import networkx as nx
+from typing import Any, Callable, Dict, List, Tuple, Union
+import random
+import json
+import itertools
+from enum import Enum
+import dataclasses
+from dataclasses import dataclass, field
+import copy
+
+
 
 INFINITY = 'INFINITY'
 WILDCARD_NUMBER = 10000
@@ -48,17 +49,21 @@ class action_label(str):
     """A basic string class to represent an action as a label."""
     pass
 
+
 class history_pattern(str):
     """A basic class to represent a subset of histories matching a sequence pattern."""
     pass
+
 
 class history(List[Tuple[observation_label, action_label]]):
     """A basic class to represent a single agent's history as a list of (observation, action) couples."""
     pass
 
-class joint_history(Dict[str, List[Union[observation_label, action_label]]]):
+
+class joint_history(Dict[str, history]):
     """A basic class to represent a joint-history."""
     pass
+
 
 class history_subset:
     """A class to represent a set of histories intended to be related to a single subset of organizational specifications for a single agent.
@@ -102,7 +107,8 @@ class history_subset:
                     observation_label, {})
                 self.history_graph[observation_label].setdefault(
                     action_label, {})
-                self.history_graph[observation_label][action_label][self.ordinal_counter] = cardinality(1, 1)
+                self.history_graph[observation_label][action_label][self.ordinal_counter] = cardinality(
+                    1, 1)
         self.ordinal_counter += 1
 
     def add_actions_to_observations(self, action_labels: List[action_label], observation_labels: List[observation_label]):
@@ -135,12 +141,13 @@ class history_subset:
                     action_label, {})
                 self.history_graph[action_label].setdefault(
                     observation_label, {})
-                self.history_graph[action_label][observation_label][self.ordinal_counter] = cardinality(1, 1)
+                self.history_graph[action_label][observation_label][self.ordinal_counter] = cardinality(
+                    1, 1)
         self.ordinal_counter += 1
 
-    def add_labels_to_labels(self, src_labels: List[Union[action_label,observation_label]],
-                             dst_labels: List[Union[action_label,observation_label]],
-                             src_to_dst_cardinality: cardinality = cardinality(1,1)):
+    def add_labels_to_labels(self, src_labels: List[Union[action_label, observation_label]],
+                             dst_labels: List[Union[action_label, observation_label]],
+                             src_to_dst_cardinality: cardinality = cardinality(1, 1)):
         """Restrict history subset to those where any of the given actions is followed by any of the given observations.
 
         Parameters
@@ -222,7 +229,7 @@ class history_subset:
         --------
         None
         """
-        
+
         def uniformize(string_pattern: str) -> str:
 
             regex = r'\[([\",0-9A-Za-z]{2,}?),[\(\[]|\),([\",0-9A-Za-z]{2,}?)\],\('
@@ -231,10 +238,11 @@ class history_subset:
             for group in matches:
                 group = group[0] if group[0] != "" else group[1]
                 if group != "":
-                    string_pattern = string_pattern.replace(group, f"([{group}],('1','1'))")
+                    string_pattern = string_pattern.replace(
+                        group, f"([{group}],('1','1'))")
             return string_pattern
 
-        def convert_to_tuple(string_pattern: str) -> Tuple[List,cardinality]:
+        def convert_to_tuple(string_pattern: str) -> Tuple[List, cardinality]:
             stack = []
             i = 0
             while i < len(string_pattern):
@@ -254,21 +262,21 @@ class history_subset:
                             i += 1
                         sequence = stack.pop()
                         sequence = f'({sequence},{card})'
-                        if(i== len(string_pattern) - 1):
-                            seq = sequence.replace("[", "[\"").replace("]","\"]") \
-                                .replace("(", "(\"").replace(")","\")").replace(",","\",\"") \
-                                .replace("\"(","(").replace(")\"",")") \
-                                .replace("\"[","[").replace("]\"","]")
+                        if (i == len(string_pattern) - 1):
+                            seq = sequence.replace("[", "[\"").replace("]", "\"]") \
+                                .replace("(", "(\"").replace(")", "\")").replace(",", "\",\"") \
+                                .replace("\"(", "(").replace(")\"", ")") \
+                                .replace("\"[", "[").replace("]\"", "]")
                             seq = uniformize(seq)
                             return eval(seq)
                         stack[-1] += sequence
                 else:
                     stack[-1] += character
                 i += 1
-        
+
         tuple_pattern = convert_to_tuple(history_pattern)
 
-        def is_only_labels(tuple_pattern: Tuple[List,cardinality]) -> bool:
+        def is_only_labels(tuple_pattern: Tuple[List, cardinality]) -> bool:
             label_or_tuple_list, card = tuple_pattern
             for label_or_tuple in label_or_tuple_list:
                 if type(label_or_tuple) == tuple:
@@ -277,7 +285,7 @@ class history_subset:
 
         self.last_label = None
 
-        def parse_into_graph(tuple_pattern: Tuple[List,cardinality]) -> None:
+        def parse_into_graph(tuple_pattern: Tuple[List, cardinality]) -> None:
 
             if is_only_labels(tuple_pattern):
 
@@ -287,11 +295,11 @@ class history_subset:
                     if self.last_label is None:
                         self.last_label = label
                     else:
-                        self.add_labels_to_labels([self.last_label],[label])
+                        self.add_labels_to_labels([self.last_label], [label])
                         self.last_label = label
-                if not(labels_card[0] == "1" and labels_card[1] == "1"):
-                    self.add_labels_to_labels([self.last_label],[labels_sequence[0]],
-                                                src_to_dst_cardinality=cardinality(labels_card[0],labels_card[1]))
+                if not (labels_card[0] == "1" and labels_card[1] == "1"):
+                    self.add_labels_to_labels([self.last_label], [labels_sequence[0]],
+                                              src_to_dst_cardinality=cardinality(labels_card[0], labels_card[1]))
                 return labels_sequence[0]
 
             else:
@@ -301,9 +309,9 @@ class history_subset:
                     sl = parse_into_graph(sub_tuple)
                     if i == 0:
                         start_label = sl
-                if not(sequences_card[0] == "1" and sequences_card[1] == "1"):
-                    self.add_labels_to_labels([self.last_label],[start_label],
-                                src_to_dst_cardinality=cardinality(sequences_card[0],sequences_card[1]))
+                if not (sequences_card[0] == "1" and sequences_card[1] == "1"):
+                    self.add_labels_to_labels([self.last_label], [start_label],
+                                              src_to_dst_cardinality=cardinality(sequences_card[0], sequences_card[1]))
 
         parse_into_graph(tuple_pattern)
 
@@ -319,8 +327,9 @@ class history_subset:
                 G.add_edge(src_label, dst_label)
                 if (src_label, dst_label) in oriented_edges.keys():
                     return
-                oriented_edges[(src_label,dst_label)] = str(self.history_graph[src_label][dst_label])
-        
+                oriented_edges[(src_label, dst_label)] = str(
+                    self.history_graph[src_label][dst_label])
+
         # Draw the graph
         pos = nx.spring_layout(G)
         fig, ax = plt.subplots()
@@ -364,27 +373,31 @@ class history_subset:
                 *reversed(fig.canvas.get_width_height()), 3)  # (H, W, 3)
             return image
 
-class os_agent_joint_history_subset:
-    """A class to represent each agent's history subset from given organizational specifications and agents."""
 
-    history_graph: Dict[Union[observation_label,action_label], Dict[Union[observation_label,action_label, Dict[os_label, Dict[int,cardinality]]]]]
+class joint_history_subset(Dict[str, history_subset]):
+    """A basic class to represent a joint history subsets."""
+    pass
+
+
+class os_to_history_subset_relations:
+    """A class to represent links between some organizational specifications and history subsets."""
+
+    history_graph: Dict[Union[observation_label, action_label],
+                        Dict[Union[observation_label, action_label], Dict[os_label, Dict[int, cardinality]]]]
 
     def __init__(self):
         self.history_graph = {}
-    
-    def add_osa_jh_relation(self, os_label: os_label, agent_number_among_all: Union[int,str], history_subsets: List[history_subset]):
-        """Add a relation between a couple (os, agent subset) to given history subsets.
+
+    def add_os_to_history_subset_relation(self, os_label: os_label, history_subsets: List[history_subset]):
+        """Add a relation between organizational specifications to corresponding history subsets.
 
         Parameters
         ----------
         os_label : os_label
             The given organizational specification label
 
-        agent_number_among_all : int | str
-            The agent subset size among all agents that can be related to given os
-        
         history_subsets: List[history_subset]
-            The history subsets that related agents are to be in
+            The history subsets related to given organizational specifications
 
         Returns
         -------
@@ -396,13 +409,58 @@ class os_agent_joint_history_subset:
         That couple is associated to a simple history subset.
         >>> h = history_subset()
         >>> h.add_actions_to_observations(["act1"],["obs1"])
-        >>> hs = os_agent_joint_history_subset()
-        >>> hs.add_osa_jh_relation("role_0", 1, [h])
+        >>> hs = os_to_history_subset_relations()
+        >>> hs.add_os_to_history_subset_relation("role_0", [h])
 
         See Also
         --------
         None
         """
+        for agent_index, history_subset in enumerate(history_subsets):
+            for src_label in history_subset.history_graph:
+                self.history_graph.setdefault(src_label, {})
+                for dst_label in history_subset.history_graph[src_label]:
+                    self.history_graph[src_label].setdefault(src_label, {})
+                    os_agent_label = os_label + "_" + "{" + str(agent_index) + "/" + str(len(history_subsets)) + "}"
+                    self.history_graph[src_label][dst_label].setdefault(os_agent_label, {})
+                    ordinal_number_to_cardinality = copy.deepcopy(history_subset.history_graph[src_label][dst_label])
+                    self.history_graph[src_label][dst_label][os_agent_label] = ordinal_number_to_cardinality
+    
+    def get_os_from_joint_history(self, joint_history: joint_history) -> Dict[str, List[os_label]]:
+        
+        joint_matching_os_labels: Dict[str, List[os_label]] = {}
+        for agent, history in joint_history.items():
+            matching_os_labels: List[List[os_label]] = []
+            for src_label, dst_label in history:
+
+                os_labels = list(self.history_graph[src_label][dst_label].keys())
+                if len(os_label) > 0 and (os_labels not in matching_os_labels):
+                    matching_os_labels += [list(set(os_labels))]
+        
+            joint_matching_os_labels[agent] = list(matching_os_labels)
+        
+        return joint_matching_os_labels
+
+    def get_joint_history_subset_from_os(self, os_label: os_label) -> joint_history_subset:
+        
+        hs: Dict[Union[observation_label, action_label],
+                        Dict[Union[observation_label, action_label], Dict[int, cardinality]]] = {}
+        for src_label in self.history_graph:
+            hs.setdefault(src_label, {})
+            for dst_label in self.history_graph[src_label]:
+                hs[src_label].setdefault(dst_label, {})
+                os_labels = self.history_graph[src_label][dst_label].keys()
+                if os_label in os_labels:
+                    hs[src_label][dst_label] = copy.deepcopy(self.history_graph[src_label][dst_label][os_label])
+        return hs
+
+    def get_next_actions_from_observation_and_os(self, observation_label: observation_label, os_label: os_label) -> joint_history_subset:
+
+        for action_label in self.history_graph.get(observation_label, {}):
+            os_labels = list(self.history_graph[observation_label][action_label].keys())
+            if os_label in os_labels:
+                return action_label
+        return None
 
 class os_factory:
 
@@ -422,6 +480,7 @@ class os_factory:
 
     def create(self) -> organizational_model:
         return self.os_model
+
 
 class history_factory:
     h_subset: history_subset
@@ -444,25 +503,23 @@ class history_factory:
     def create(self) -> history_subset:
         return self.h_subset
 
+
 class joint_history_factory:
 
-    jh_subset: os_agent_joint_history_subset
+    jh_subset: os_to_history_subset_relations
 
     def __init__(self) -> None:
         self.jh_subset = None
 
     def new(self) -> 'joint_history_factory':
-        self.jh_subset = os_agent_joint_history_subset()
+        self.jh_subset = os_to_history_subset_relations()
         return self
 
     def add_a_history_subset(self, agents_number_among_all: int, history_subsets: List[history_subset]) -> 'joint_history_factory':
         return self
 
-    def create(self) -> os_agent_joint_history_subset:
+    def create(self) -> os_to_history_subset_relations:
         return self.jh_subset
-
-
-
 
 
 OSF = os_factory()
@@ -486,7 +543,7 @@ class osh_manager():
 
     Methods
     -------
-    create_relation(self, organizational_model: organizational_model, os_agent_joint_history_subset: os_agent_joint_history_subset) -> 'osh_manager':
+    create_relation(self, organizational_model: organizational_model, os_to_history_subset_relations: os_to_history_subset_relations) -> 'osh_manager':
         Initiates the relation from organizational specifications (under the form of an organizational) to a subset of joint histories.
     """
 
@@ -498,7 +555,7 @@ class osh_manager():
     def __init__(self) -> None:
         self.osh_graph = {}
 
-    def create_relation(self, organizational_model: organizational_model, os_agent_joint_history_subset: os_agent_joint_history_subset) -> 'osh_manager':
+    def create_relation(self, organizational_model: organizational_model, os_to_history_subset_relations: os_to_history_subset_relations) -> 'osh_manager':
         """Initiates the relation from organizational specifications (under the form of an organizational) to a subset of joint histories.
 
         Parameters
@@ -506,7 +563,7 @@ class osh_manager():
         organizational_model : organizational_model
             An organizational model describing organizational specifications
 
-        os_agent_joint_history_subset: os_agent_joint_history_subset
+        os_to_history_subset_relations: os_to_history_subset_relations
             A subset of joint-histories describing how a subset of agent adopting the organizational specifications should behave
 
         Returns
@@ -522,7 +579,7 @@ class osh_manager():
                 organizational_model=OSF.new()
                 .add_role("Role_0")
                 .create(),
-                os_agent_joint_history_subset=JHF.new()
+                os_to_history_subset_relations=JHF.new()
                 .add_a_history_subset(
                     agents_number_among_all=1,
                     history_subsets=[HF.new()
@@ -543,16 +600,17 @@ if __name__ == '__main__':
 
     hs = history_subset()
     # hs.add_pattern("[obs1,[act2,obs2](1,2),act3](1,*)")
-    hs.add_pattern("[obs1,act1,[obs2,act2,[obs3,act3](2,2),[obs14,[act45,obs78](0,*),act15](14,12),[obs3,act3](2,2),obs4,act4](1,2)](1,*)")
+    hs.add_pattern(
+        "[obs1,act1,[obs2,act2,[obs3,act3](2,2),[obs14,[act45,obs78](0,*),act15](14,12),[obs3,act3](2,2),obs4,act4](1,2)](1,*)")
     # hs.plot_graph(show=True)
 
     hs = history_subset()
-    hs.add_actions_to_observations(["obs1"],["act1"])
-    hs.add_actions_to_observations(["obs2"],["act2"])
+    hs.add_actions_to_observations(["obs1"], ["act1"])
+    hs.add_actions_to_observations(["obs2"], ["act2"])
     # hs.plot_graph(show=True)
 
     hs = history_subset()
-    hs.add_history([("obs1","act1"),("obs2","act2"),("obs3","act3")])
+    hs.add_history([("obs1", "act1"), ("obs2", "act2"), ("obs3", "act3")])
     # hs.plot_graph(show=True)
 
     # oshr = osh_manager()
@@ -561,7 +619,7 @@ if __name__ == '__main__':
     #     organizational_model=OSF.new()
     #     .add_role("Role_0")
     #     .create(),
-    #     os_agent_joint_history_subset=JHF.new()
+    #     os_to_history_subset_relations=JHF.new()
     #     .add_a_history_subset(
     #         agents_number_among_all=1,
     #         history_subsets=[HF.new()
