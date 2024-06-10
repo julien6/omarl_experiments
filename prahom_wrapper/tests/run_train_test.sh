8467d3e3b4b8be12bf67cdaec8f11599cb085c4c
julien soulé <julien.soule@lcis.grenoble-inp.fr>
2024-06-10 03:05:37 +0200

    corrected training launcher command

--- prahom_wrapper/tests/run_train_test.sh
+++ prahom_wrapper/tests/run_train_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#OAR -n Pistonball
+#OAR -n MPE
 #OAR -l /nodes=1/gpu=1,walltime=4:00:00
 #OAR --stdout %jobid%.out
 #OAR --stderr %jobid%.err
@@ -9,4 +9,4 @@
 
 cd /bettik/soulej/omarl_experiments/
 source prahom_wrapper/tests/prahom_test/bin/activate
-python3 prahom_wrapper/tests/marllib_basic_use.py
+python prahom_wrapper/tests/marllib_basic_use.py
