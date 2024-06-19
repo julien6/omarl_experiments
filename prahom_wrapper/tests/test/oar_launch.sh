6cb7b24a169cb1d849348388cf32f0024950623f
Julien <julien.soule@lcis.grenoble-inp.fr>
2024-06-19 18:46:07 +0200

    finalized training framework

--- prahom_wrapper/tests/test/oar_launch.sh
+++ prahom_wrapper/tests/test/oar_launch.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+if [ -e ./tmp_job.sh ]; then
+    rm -rf tmp_job.sh
+fi
+
+WALLTIME=$1
+if [ -z "$1" ]; then
+    WALLTIME="0:30:00"
+fi
+
+cat <<EOT > tmp_job.sh
+#!/bin/bash
+
 #OAR -n Tests
-#OAR -l /nodes=1/gpu=1,walltime=4:00:00
+#OAR -l /nodes=1/gpu=1,walltime=$WALLTIME
 #OAR --stdout %jobid%.out
 #OAR --stderr %jobid%.err
 #OAR --project pr-ai4cmas
 #OAR -p gpumodel='V100'
+
+echo "Job started at $(date)"
+echo "Current directory: $(pwd)"
+echo "List of files in current directory:"
+ls -la
 
 if [ -e "/applis/environments/conda.sh" ]; then
     source /applis/environments/conda.sh
@@ -13,6 +30,83 @@ else
     source ~/anaconda3/etc/profile.d/conda.sh
 fi
 
+conda info --envs
+echo "Activating marllib environment"
 conda activate marllib
 
-python train_test.py
+if [ $? -ne 0 ]; then
+    echo "Error during conda activation"
+    exit 1
+fi
+
+conda info --envs
+
+which python
+python --version
+
+echo "Starting train_test.py"
+python -u train_test.py
+
+if [ $? -ne 0 ]; then
+    echo "Error during train_test.py execution"
+    exit 1
+fi
+
+echo "Job ended at $(date)"
+EOT
+
+chmod +x tmp_job.sh
+rsync -avxH tmp_job.sh soulej@bigfoot.ciment:/bettik/soulej/omarl_experiments/prahom_wrapper/tests/test/
+ssh soulej@bigfoot.ciment -t "cd /bettik/soulej/omarl_experiments/prahom_wrapper/tests/test ; rm -rf *.err *.out exp_results ; oarsub -S ./tmp_job.sh"
+mkdir exp_results
+
+convert_to_seconds() {
+    local TIME=$1
+    local IFS=:
+    local PARTS=($TIME)
+    local HOURS=${PARTS[0]}
+    local MINUTES=${PARTS[1]}
+    local SECONDS=${PARTS[2]}
+    echo $((HOURS * 3600 + MINUTES * 60 + SECONDS))
+}
+
+show_progress_bar() {
+  local DURATION=$1
+  local INTERVAL=1
+  local ELAPSED=0
+  local BAR_SIZE=50
+
+  while [ $ELAPSED -lt $DURATION ]; do
+    local PROGRESS=$(( ELAPSED * BAR_SIZE / DURATION ))
+    local PERCENT=$(( ELAPSED * 100 / DURATION ))
+    local BAR=''
+
+    for ((i=0; i<PROGRESS; i++)); do
+      BAR+='='
+    done
+
+    for ((i=PROGRESS; i<BAR_SIZE; i++)); do
+      BAR+=' '
+    done
+
+    printf "\r[%s] %d%%" "$BAR" "$PERCENT"
+
+    sleep $INTERVAL
+    ELAPSED=$(( ELAPSED + INTERVAL ))
+  done
+
+  # Afficher 100% lorsque la durée est terminée
+  printf "\r[%s] 100%%\n" $(printf '=%.0s' {1..50})
+}
+
+echo "WAITING FOR TRAINING TO FINISH..."
+
+SECONDS_DURATION=$(convert_to_seconds $WALLTIME)
+# sleep $SECONDS_DURATION + 60
+show_progress_bar $SECONDS_DURATION + 60
+
+echo "\nTRAINING FINISHED"
+
+rsync -avxH soulej@bigfoot.ciment:/bettik/soulej/omarl_experiments/prahom_wrapper/tests/test/exp_results/ ./exp_results/
+ssh soulej@bigfoot.ciment -t "cd /bettik/soulej/omarl_experiments/prahom_wrapper/tests/test ; rm -rf tmp_job.sh"
+rm -rf tmp_job.sh
--- prahom_wrapper/tests/test/train_test.py
+++ prahom_wrapper/tests/test/train_test.py
@@ -8,7 +8,7 @@ from datetime import datetime
 # prepare the environment academy_pass_and_shoot_with_keeper
 # env = marl.make_env(environment_name="hanabi", map_name="Hanabi-Very-Small")
 env = marl.make_env(environment_name="mpe",
-                    map_name="simple_spread", force_coop=True)
+                    map_name="simple_world_comm", force_coop=True)
 
 # can add extra env params. remember to check env configuration before use
 # env = marl.make_env(environment_name='smac', map_name='3m', difficulty="6", reward_scale_rate=15)
@@ -25,9 +25,8 @@ model = marl.build_model(
 
 checkpoint_freq = 500
 
-# MAPPOTrainer_mpe_simple_spread_0c232_00000_0_2024-06-16_21-17-18/tmpb1_j4jrnrestore_from_object/./.tune_metadata
-
-# MAPPOTrainer_mpe_simple_spread_0c232_00000_0_2024-06-16_21-17-18/tmpb1_j4jrnrestore_from_object
+# MAPPOTrainer_mpe_simple_world_comm_0c232_00000_0_2024-06-16_21-17-18/tmpb1_j4jrnrestore_from_object/./.tune_metadata
+# MAPPOTrainer_mpe_simple_world_comm_0c232_00000_0_2024-06-16_21-17-18/tmpb1_j4jrnrestore_from_object
 
 if len(sys.argv) > 1 and sys.argv[1] == "--test":
 
@@ -72,8 +71,10 @@ if len(sys.argv) > 1 and sys.argv[1] == "--test":
     best_checkpoint_dir = [p for p in Path(best_logdir).iterdir(
     ) if "checkpoint_" in p.name and (int(p.name.split("checkpoint_")[1]) <= training_iteration and training_iteration <= int(p.name.split("checkpoint_")[1]) + checkpoint_freq)][0]
 
-    checkpoint_number = str(int(best_checkpoint_dir.name.split("checkpoint_")[1]))
-    best_checkpoint_file_path = os.path.join(best_checkpoint_dir, f'checkpoint-{checkpoint_number}')
+    checkpoint_number = str(
+        int(best_checkpoint_dir.name.split("checkpoint_")[1]))
+    best_checkpoint_file_path = os.path.join(
+        best_checkpoint_dir, f'checkpoint-{checkpoint_number}')
 
     # rendering
     mappo.render(env, model,
